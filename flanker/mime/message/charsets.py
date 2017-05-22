b7899ee0158f0589645992ae41c19c826623aebd
Haiyuan Cao <haca@microsoft.com>
2017-05-22 13:48:55 -0700

    update check of unicode to check of string in Python 3

--- flanker/mime/message/charsets.py
+++ flanker/mime/message/charsets.py
@@ -3,7 +3,7 @@ from flanker.mime.message.utils import to_unicode
 
 def convert_to_unicode(charset, value):
     #in case of unicode we have nothing to do
-    if isinstance(value, unicode):
+    if isinstance(value,str):
         return value
 
     charset = _translate_charset(charset)
